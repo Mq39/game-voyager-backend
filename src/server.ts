ff717a9d8c6573c55b4a1cf4222ef3fa4f12f48d
Mq39 <predragmitr@gmail.com>
2026-03-17 19:09:46 +0100

    Final

--- src/server.ts
+++ src/server.ts
@@ -50,9 +50,7 @@ app.use(cors({
         "http://localhost:5173",
         "https://game-voyager.vercel.app"
     ],
-    credentials: true,
-    methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    credentials: true
 }))
 app.use(morgan("dev"))
 app.use(express.json())
